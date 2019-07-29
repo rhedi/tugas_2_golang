c7c38a875732726898f50da4c722ee818e2b15e3
Rhedi <rhedipratama@gmail.com>
2019-07-29 11:20:13 +0700

    Tugas 2 golang
    tugas ini saya kumpulkan untuk niomic

---
+++ tugas2.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main()  {
+  var angka = ((1/1) %2)
+
+  switch angka {
+  case 9:
+    fmt.Println("ganjil")
+  case 7:
+    fmt.Println("ganjil")
+  case 5:
+    fmt.Println("ganjil")
+  case 3:
+    fmt.Println("ganjil")
+  case 1:
+    fmt.Println("ganjil")
+  default:
+    fmt.Println("genap")
+}
+}
